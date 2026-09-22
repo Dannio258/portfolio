c37ef5208b02532f0adae56caee1f0f3fa1a1ebf
Daniels Kuzmins <daniels.kuzmins04@gmail.com>
2026-09-22 10:22:09 +0200

    Finilized the Navbar

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/contant",
+    path: "/contact",
     element: <App />,
   },
   {
--- src/pageComponents/Navbar.tsx
+++ src/pageComponents/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import type { Dispatch, SetStateAction } from "react";
 import ModeToggle from "../components/ModeToggle";
 
@@ -8,35 +8,35 @@ type NavbarProps = {
 };
 
 function Navbar({ darkMode, setDarkMode }: NavbarProps) {
+  const navLinkStyle = ({ isActive }: { isActive: boolean }) =>
+  `transition-all duration-300 px-3 py-2 rounded-md ${
+    isActive
+      ? "dark:bg-accent bg-blue-700 text-text-dark"
+      : "text-text dark:text-text-dark hover:bg-slate-100 dark:hover:bg-slate-800"
+  }`;
   return (
     <>
-      <nav className="w-[90%] sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-auto flex py-3 px-4 items-center outline-slate-200 outline-1 rounded-xl dark:bg-surface-dark text-text dark:text-text-dark">
+      <nav className="w-[90%] sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-auto text-sm md:text-lg lg:text-xl flex py-3 px-4 items-center outline-slate-200 outline-1 rounded-xl dark:bg-surface-dark text-text dark:text-text-dark">
         <p className="text-2xl">DK</p>
         <div className="flex items-center ml-auto gap-10">
-          <ul>
+          <ul className="flex items-center gap-10">
             <li>
-              <Link to={"/"}>Home</Link>
+              <NavLink className={navLinkStyle} to={"/"}>Home</NavLink>
             </li>
-          </ul>
-          <ul>
             <li>
-              <Link to={"/about"}>About</Link>
+              <NavLink className={navLinkStyle} to={"/about"}>About</NavLink>
             </li>
-          </ul>
-          <ul>
             <li>
-              <Link to={"/projects"}>Projects</Link>
+              <NavLink className={navLinkStyle} to={"/projects"}>Projects</NavLink>
             </li>
-          </ul>
-          <ul>
             <li>
-              <Link to={"/contant"}>Contact</Link>
+              <NavLink className={navLinkStyle} to={"/contact"}>Contact</NavLink>
             </li>
           </ul>
           <button onClick={() => setDarkMode((prev) => !prev)}>
             <ModeToggle
               className={`size-8 transition-transform duration-300 ${
-                darkMode ? "text-accent rotate-270" : "text-slate-500 rotate-0"
+                darkMode ? "text-accent rotate-270" : "text-blue-700 rotate-0"
               }`}
             />
           </button>
